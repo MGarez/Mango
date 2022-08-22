baa1131561d415c932d81d942a9805b51398d898
Mat Garez <matgarez@gmail.com>
2022-08-22 19:30:15 +0200

    Work on NewProjectView property bindings

---
+++ MangoEditor/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangoEditor
+{
+	internal class ViewModelBase : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		protected void OnPropertyChanged(string PropertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+		}
+	}
+}
